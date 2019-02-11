718f52025b652fc8a13ca218a40c4c99186d6b76
Henry Coulson <h.coulson@outlook.com>
2019-02-11 14:31:10 +1300

    Added script for taks dropdown menu. Needs work

---
+++ TaskSelectorScript.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskSelectorScript : MonoBehaviour {
+
+
+    Dropdown dropdown;
+    Dictionary<int,string> entries = new Dictionary<int, string> {
+    { 0, "Tasks" },
+    { 1, "Dig Soil" },
+    { 2, "Plant" },
+    { 3, "Mulch"},
+    { 4, "Remove Topsoil"},
+    { 5, "Add Topsoil"}
+    };
+
+    // Use this for initialization
+    void Start () {
+		dropdown = GetComponent<Dropdown>();
+        dropdown.itemText.text = "Tasks";
+
+        dropdown.ClearOptions();
+
+        for (int i =0; i< entries.Count;i++) {
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            string value;
+            entries.TryGetValue(i, out value);
+            option.text = value;
+            dropdown.options.Add(option);
+        }
+
+        dropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(dropdown);
+        });
+
+    }
+
+    private void DropdownValueChanged(Dropdown dropdown) {
+        int selectedIndex = dropdown.value;
+        string selectedText = dropdown.options[selectedIndex].text;
+        selectedText = selectedText.Replace(" ", "");
+
+        //use reflection to run the method associated with the text;
+        Type type = this.GetType();
+        MethodInfo method = type.GetMethod(selectedText);
+
+    }
+}
